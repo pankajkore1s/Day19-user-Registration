430ec3c24d55508eea66e09b4c6ff397a6f6cf6d
pankaj kore <korep95@gmail.com>
2022-02-23 20:49:30 +0530

    [pankaj]added feature of validating last name

--- src/com/BridgeLabz/UserRegistration/UserRegistrationValidation.java
+++ src/com/BridgeLabz/UserRegistration/UserRegistrationValidation.java
@@ -4,7 +4,6 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationValidation {
     static Scanner sc = new Scanner(System.in);
-
     public static void validateFirstName() {
         System.out.println("Enter First name ");
         String first_name = sc.nextLine();
@@ -16,9 +15,21 @@ public class UserRegistrationValidation {
             System.out.println("'" + first_name + "'" + " validation unsuccessful!");
         }
     }
-
-    public static void main(String[] args) {
+    public static void validateLastName() {
+        System.out.println("Enter Last name ");
+        String last_name = sc.nextLine();
+        String lastNamePattern = "^[A-Z]{1}[a-z,A-Z]{2,}";
+        Pattern pattern = Pattern.compile(lastNamePattern);
+        if ((last_name.matches(lastNamePattern))) {
+            System.out.println("'" + last_name + "'" + " validation success!");
+        } else {
+            System.out.println("'" + last_name + "'" + " validation unsuccessful!");
+        }
+    }
+    public static void main (String[] args ){
         System.out.println("Welcome to User Registration Project ");
         validateFirstName();
+        validateLastName();
     }
 }
+
